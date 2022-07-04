f53ee1e6c67d557e84129de0cc6bd9f0a22edef3
MAROUN <marounjassaf@gmail.com>
2022-07-04 23:40:33 +0300

    Setup Environement from UMS DB

--- UMS.Domain/Models/Role.cs
+++ UMS.Domain/Models/Role.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using PCP.Persistence.Models;
+
 
 namespace PCP.Persistence.Models
 {
@@ -10,7 +13,14 @@ namespace PCP.Persistence.Models
             Users = new HashSet<User>();
         }
 
+        public Role(long id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        [Required]
         public long Id { get; set; }
+        [Required]
         public string Name { get; set; } = null!;
 
         public virtual ICollection<User> Users { get; set; }
